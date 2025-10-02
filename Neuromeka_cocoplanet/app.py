34ed525fec847b25bb9bbb5ee92609560aa30158
zxxnch <138439783+zxxnch@users.noreply.github.com>
2025-10-02 17:46:05 +0900

    Add subprocess to launch Firefox in kiosk mode after starting Flask server; include new background image

--- Neuromeka_cocoplanet/app.py
+++ Neuromeka_cocoplanet/app.py
@@ -5,6 +5,7 @@ import random
 import signal
 import threading
 import grpc
+import subprocess
 from google.protobuf import empty_pb2
 from flask import Flask, render_template, jsonify
 
@@ -151,14 +152,66 @@ def index():
 def get_status():
     return jsonify(latest_data)
 
-
 if __name__ == "__main__":
     stop_event = threading.Event()
     t = threading.Thread(target=grpc_loop, args=(stop_event,), daemon=True)
     t.start()
 
     try:
-        app.run(debug=True, use_reloader=False, port=5050)
+        # Flask 서버 실행을 별도 스레드에서 돌림
+        app_thread = threading.Thread(
+            target=lambda: app.run(debug=True, use_reloader=False, port=5050),
+            daemon=True
+        )
+        app_thread.start()
+
+        # 서버 뜰 때까지 잠시 대기 후 Firefox 키오스크 실행
+        time.sleep(3)
+        subprocess.Popen([
+            "firefox",
+            "--kiosk",
+            "http://localhost:5050"
+        ])
+
+        app_thread.join()
+
     finally:
         stop_event.set()
         t.join()
+# if __name__ == "__main__":
+#     stop_event = threading.Event()
+#     t = threading.Thread(target=grpc_loop, args=(stop_event,), daemon=True)
+#     t.start()
+
+#     try:
+#         # Flask 서버 실행
+#         app_thread = threading.Thread(
+#             target=lambda: app.run(debug=True, use_reloader=False, port=5050),
+#             daemon=True
+#         )
+#         app_thread.start()
+#         # subprocess.Popen([
+#         #     r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+#         #     "--start-fullscreen",   # 크롬은 --kiosk 대신 이걸 권장
+#         #     "--disable-infobars",
+#         #     "http://localhost:5050"
+#         # ])
+
+#         # # Flask 서버가 켜질 때까지 잠깐 대기 후 브라우저 실행
+#         # time.sleep(2)
+#         # import subprocess
+#         subprocess.Popen([
+#             "chromium-browser",
+#             "--noerrdialogs",
+#             "--disable-infobars",
+#             "--kiosk",
+#             "http://localhost:5050"
+#         ])
+
+#         # 메인 스레드는 대기
+#         app_thread.join()
+
+#     finally:
+#         stop_event.set()
+#         t.join()
+
